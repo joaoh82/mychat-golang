db2a02887618de26ffead8cefdc65bc21c9493b4
Joao Henrique Machado Silva <joaoh82@gmail.com>
2016-10-13 15:24:45 -0300

    Host and guest connecting and sending one message

---
+++ lib/main.go
@@ -0,0 +1,52 @@
+package lib
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+const port = "8080"
+
+// RunHost takes an ip as an argument
+// and listens for connections on that ip
+func RunHost(ip string) {
+	ipAndPort := ip + ":" + port
+	listener, listenErr := net.Listen("tcp", ipAndPort)
+	if listenErr != nil {
+		log.Fatal("Error: ", listenErr)
+	}
+	fmt.Println("Listening on", ipAndPort)
+
+	conn, acceptErr := listener.Accept()
+	if acceptErr != nil {
+		log.Fatal("Error: ", acceptErr)
+	}
+	fmt.Println("New connection accepted")
+
+	reader := bufio.NewReader(conn)
+	message, readErr := reader.ReadString('\n')
+	if readErr != nil {
+		log.Fatal("Error: ", readErr)
+	}
+	fmt.Println("Message received: ", message)
+}
+
+// RunGuest takes an destination ip as an argument
+// and connects to that ip
+func RunGuest(ip string) {
+	ipAndPort := ip + ":" + port
+	conn, dialErr := net.Dial("tcp", ipAndPort)
+	if dialErr != nil {
+		log.Fatal("Error: ", dialErr)
+	}
+	fmt.Print("Send message: ")
+	reader := bufio.NewReader(os.Stdin)
+	message, readErr := reader.ReadString('\n')
+	if readErr != nil {
+		log.Fatal("Error: ", readErr)
+	}
+	fmt.Fprint(conn, message)
+}
